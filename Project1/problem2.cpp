2576449ffbf3cc2e24dac332167cd7fbfda38fea
Oskar Idland <oskaridland@hotmail.com>
2023-08-25 14:20:22 +0200

    touched up main

--- Project1/problem2.cpp
+++ Project1/problem2.cpp
@@ -3,21 +3,17 @@
 #include <armadillo>
 #include <string>
 #include <fstream>
-#include <chrono>
 #include "problem2.h"
 
 using namespace std;
+using arma::vec;
 
 double u (double x){
     return 1 - (1-exp(-10))*x - exp(-10*x);
 }
 
 void problem2(){
-    // Start measuring time
-    auto t1 = chrono::high_resolution_clock::now();
-
-    arma::vec x {arma::linspace<arma::vec>(0.0, 1.0, 100)};
-
+    vec x {arma::linspace<vec>(0.0, 1.0, 100)};
 
     string filename = "x_u.txt";
     ofstream ofile;
@@ -29,14 +25,4 @@ void problem2(){
               << endl;
     }
     ofile.close();
-
-    // Stop measuring time
-    auto t2 = chrono::high_resolution_clock::now();
-
-    // Calculate the elapsed time
-    // We use chrono::duration<double>::count(), which by default returns duration in seconds
-    double duration_seconds = chrono::duration<double>(t2 - t1).count();
-
-    // Print the elapsed time
-    cout << duration_seconds << " s" << endl;
 }
