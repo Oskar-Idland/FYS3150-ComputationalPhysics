a1ed74e9fae3da7d5aa079960fea2368c37e483e
hishemok <hishemok@uio.no>
2023-08-25 15:29:12 +0200

    rm problem2 + clean i makefile

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <iomanip>
 #include <armadillo>
-#include "problem2.h"
+#include "u.h"
+#include "write_to_file.h"
 
 using namespace std;
+using arma::vec;
 int main()
 {
   // Print a message to screen
   cout << "Hello, World!\n";
-  problem2(); 
-  // Return 0 when done
+  
+  //------Problem 2------
+  vec x {arma::linspace<vec>(0.0, 1.0, 100)};
+  vec v {u(x)};
+  write_to_file(x, v, "x_u.txt");
+  
   return 0;
 }
