7c9b2b73ca13c254c68feccd36ab78d2142e62a7
Oskar Idland <oskaridland@hotmail.com>
2023-08-25 18:10:07 +0200

    Begun proble 7. Code does NOT work

---
+++ Project1/f.cpp
@@ -0,0 +1,7 @@
+#include "f.h"
+#include <cmath>
+#include <armadillo>
+
+arma::vec f(arma::vec x) {
+    return 100 * exp(-10 * x);
+}
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 #include <armadillo>
-// #include "problem2.h"
-#include "A.h"
-#include "u.h"
 #include "write_to_file.h"
+#include "u.h"
+#include "f.h"
 
 using namespace std;
-using arma::vec;
+using arma::vec, arma::linspace, arma::mat;
+
 int main()
-{
-  // Print a message to screen
-  cout << "Hello, World!\n";
-  
+{ 
   //------Problem 2------
-  vec x {arma::linspace<vec>(0.0, 1.0, 100)};
+  vec x {arma::linspace<vec>(0.0, 1.0, 4)};
   vec v {u(x)};
-  write_to_file(x, v, "x_u.txt");
+  // write_to_file(x, v, "x_u.txt");
+
+  //------Problem 7-------
+  double ddx2 {std::pow(x.at(1) - x.at(0), 2)};
+  vec g {- f(x) * ddx2};
   
+  mat A (x.n_elem, x.n_elem, arma::fill::zeros);
+  A.diag() += 2;
+  A.diag(1) += -1;
+  A.diag(-1) += -1;
+
+  A.insert_cols(x.n_elem, g);
+
+  cout << A << endl;
+    
   return 0;
 }
